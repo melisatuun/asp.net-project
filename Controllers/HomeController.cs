675881d62f92597c9a0840aa28849896539f6a67
Melo <63464796+melisatuun@users.noreply.github.com>
2022-06-14 17:25:55 +0300

    first commit
    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment he;
+
+        public HomeController(IHostingEnvironment e) {
+            he = e;
+        }
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+            //_logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //picture upload function
+        public IActionResult ShowFields(string fullName, IFormFile pic) 
+        {
+            ViewData["fname"] = fullName;
+            if (pic != null) {
+                var fileName = Path.Combine(he.WebRootPath, Path.GetFileName(pic.FileName));
+                pic.CopyTo(new FileStream(fileName, FileMode.Create));
+                ViewData["fileLocation"] = "/"+Path.GetFileName(pic.FileName);
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
